2b8563900d2afaa9eb7367b3e718bbb5586240b7
Jay <jenjentw358@gmail.com>
2024-01-01 20:49:36 -0500

    test

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Hero = () => {
+    return(<section>
+    <h1 className="text-white"></h1>
+    </section>)
+}
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
-  return (
+import React from 'react';
+import Hero from './components/Hero';
+const Home = () => {
+    return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className={"text-2xl font-bold"}>Hi</h1>
+        <Hero />
     </main>
   )
 }
+export default Home;
